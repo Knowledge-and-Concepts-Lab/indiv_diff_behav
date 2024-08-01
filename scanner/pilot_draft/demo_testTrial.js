2f6d8740e5d60b5e13ff6dfcf46e59db03a63d45
Ivette Colón <53277957+ivettecolon@users.noreply.github.com>
2024-08-01 09:17:46 -0500

    changing directory organization

--- scanner/pilot_draft/demo_testTrial.js
+++
@@ -1,5 +0,0 @@
-// I essentially need three different trial sequences:
-
-// 1) I need the normal picture + fixation for the birds, mammals, and marine animals
-// 2) I need the n-back task (sequence of 6 numbers, three the same)
-// 3) a test trial after a certain number of rounds that is an array of 
